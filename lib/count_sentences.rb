4bf6ea56580c09a05f703cabd519fd45f5e33640
Joseph Martin <josephmartin1350@gmail.com>
2022-02-24 17:21:22 -0700

    commit tree 028f4cef986591409520ee3a7e837bfa8044a6ce

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "."
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentences = 0
+    punctuation = [".", "?", "!"]
+    self.chars.each.with_index do |char, i|
+      if punctuation.include?(char) && !punctuation.include?(self[i + 1])
+        sentences += 1
+      end
+    end
+    sentences
   end
+  
 end
